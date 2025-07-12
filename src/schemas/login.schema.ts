a6e66d5f0e8ee603ff57f2296fa073ae3cfa7a1b
Tryboy869 <anzizdaouda0@gmail.com>
2025-07-12 09:51:56 +0100

    Delete src directory

--- src/schemas/login.schema.ts
+++
@@ -1,8 +0,0 @@
-import { z } from "zod";
-
-export const loginSchema = z.object(
-    {
-        identifier: z.string(),
-        password: z.string().min(8, "Password must be at least 8 characters"),
-    }
-);
